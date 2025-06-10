f741debc858d9ce62a28eca5de57f47397d83fbf
YasmimLivon <yasmim.livon@aluno.senai.br>
2025-06-10 17:24:33 -0300

    lista de tarefas funcional

--- script.js
+++ script.js
@@ -9,5 +9,28 @@ titulo.innerHTML = `Lista de tarefas: ${nome}`;
 
 btnadd.addEventListener("click", criatarefa);
 function criatarefa(){
-    
+  const listItem = document.createElement('li')
+    listItem.textContent = tarefa.value;
+    tasklist.appendChild(listItem);
+    const removebutton = document.createElement('button');
+    removebutton.id = "remove"
+    removebutton.textContent = "x"
+    removebutton.addEventListener("click", function(){
+        tasklist.removeChild(listItem);
+    })
+    const concluirbutton = document.createElement('button');
+    concluirbutton.id = "conclui"
+    concluirbutton.textContent = "✓"
+
+    let buttonsItem = document.createElement('div');
+    buttonsItem.classList.toggle('buttonsItem')
+    listItem.appendChild(buttonsItem);
+    buttonsItem.appendChild(concluirbutton);
+    buttonsItem.appendChild(removebutton);
+
+    concluirbutton.addEventListener("click", function(){
+        listItem.classList.toggle("completed")
+    })
+
+  
 }
